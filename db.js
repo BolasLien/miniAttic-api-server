584cc62e6c1d1508c7ad61c9435b075901a3a42d
BolasLien <ltzuen@gmail.com>
2020-07-13 15:53:05 +0800

    新增頁面設定的資料表

--- db.js
+++ db.js
@@ -41,12 +41,52 @@ const fileSchema = new Schema({
   versionKey: false
 })
 
+const pageSchema = new Schema({
+  path: {
+    // 例如: home 或 about
+    type: String,
+    required: [true, '沒有路徑']
+  },
+  area: {
+    // 例如: carousel 或 intro
+    type: String,
+    required: [true, '沒有區塊']
+  },
+  item: {
+    // 例如: carousel-item-1 或 intro-item-1
+    type: String,
+    required: [true, '沒有指定item']
+  },
+  filepath: {
+    // 例如: /images/xxx.jpg 或 /miniattic/assets/img/xxx.jpg
+    type: String,
+    required: [true, '沒有檔案路徑']
+  },
+  description: {
+    type: String
+  },
+  link: {
+    // 例如: http://google.com/ / /about
+    type: String
+  },
+  isShow: {
+    type: Boolean,
+    required: [true, '沒有設定是否顯示'],
+    default: true
+  }
+}, {
+  versionKey: false
+}
+)
+
 const users = mongoose.model(process.env.COLLECTION_USER, userSchema)
 const files = mongoose.model(process.env.COLLECTION_FILE, fileSchema)
+const pages = mongoose.model(process.env.COLLECTION_PAGE, pageSchema)
 const connection = mongoose.connection
 
 export default {
   users,
   files,
+  pages,
   connection
 }
