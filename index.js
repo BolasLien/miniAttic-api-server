4a6122eac3bedbb68626b8bb8435fbc859def11f
BolasLien <ltzuen@gmail.com>
2020-08-03 13:20:36 +0800

    cookies

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(session({
     // 1000 毫秒 * 60 = 一分鐘
     // 1000 毫秒 * 60 * 30 = 三十分鐘
     maxAge: 1000 * 60 * 30,
-    domain: process.env.ALLOW_CORS ? 'localhost' : '.github.io'
+    sameSite: process.env.ALLOW_CORS
   },
   resave: true,
   // 是否保存未修改的session
