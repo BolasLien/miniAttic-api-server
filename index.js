4a2b0df364527517f61891b2773244ff22de9acd
BolasLien <ltzuen@gmail.com>
2020-08-03 13:33:29 +0800

    cookie
        domain: 'bolaslien.github.io',
        sameSite: 'none',
        secure: 'auto'

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(session({
     // 1000 毫秒 * 60 = 一分鐘
     // 1000 毫秒 * 60 * 30 = 三十分鐘
     maxAge: 1000 * 60 * 30,
-    domain: process.env.ALLOW_CORS ? 'localhost' : 'bolaslien.github.io',
+    domain: 'bolaslien.github.io',
     sameSite: 'none',
     secure: 'auto'
   },
